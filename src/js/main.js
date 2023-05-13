b7237af074e2444bc9b6ef221ea17deca99c0fc4
Ronaldo Junior <ronaldomjunior05@gmail.com>
2023-05-13 11:52:11 -0300

    select 'accessibility' done

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 const select = document.getElementsByClassName('js-select')[0]
 const selected = document.getElementsByClassName('js-selected')[0]
+const options = document.getElementsByClassName('js-options')[0]
+let timesSelected = 0
 
-select.addEventListener('click', e => {
+const selectedToZero = () => {
+    timesSelected = 0
+}
+
+const selectActions = e => {
     const element = e.target
+    const key = e.key
     const action = element.getAttribute('data-action')
 
     if(!action) return
@@ -10,6 +17,12 @@ select.addEventListener('click', e => {
     const actions = {
         selected(){
             select.classList.toggle('active')
+            timesSelected += 1
+
+            if(timesSelected === 2){
+                selected.blur()
+                selectedToZero()
+            }
         },
         option(){
             selected.value = element.textContent
@@ -17,10 +30,26 @@ select.addEventListener('click', e => {
         }
     }
 
-    if(actions[action]) actions[action]()
+    if(actions[action] && (key === "Enter" || key === undefined)) actions[action]()
 
-})
+}
+
+select.addEventListener('click', selectActions)
+
+select.addEventListener('keypress', selectActions)
 
 selected.addEventListener('blur', () => {
     select.classList.remove('active')
+    selectedToZero()
 })
+
+for(let i = 0; i < options.children.length; i++){
+    options.children[i].addEventListener('focus', () => {
+        select.classList.add('active')
+    })
+        
+    options.children[i].addEventListener('blur', () => {
+        select.classList.remove('active')
+        selectedToZero()
+    })
+}
